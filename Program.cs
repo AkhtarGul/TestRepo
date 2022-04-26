e31d8982f9f87c635b9663a0cecebe53267fb2e6
Akhtar Gul <akhtar0316169@gmail.com>
2022-04-26 18:24:07 +0500

    fsdf

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+bool equal = Calclutors.AreEqual(1,2);
+
+if (equal)
+{
+    Console.WriteLine("Equal");
+}
+else
+{
+    Console.WriteLine("Not Equal");
+}
+public   class Calclutors
+{
+    public static bool AreEqual<T>(T r1,T r2)
+    {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+        return r1.Equals(r2);
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+    }
+}
+
